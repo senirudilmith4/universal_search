b2f588088541ebb6b61f850da19c9eb1cca33a66
senirudilmith4 <dilmith.seniru0@gmail.com>
2026-09-02 14:14:08 +0530

    Folder setup

---
+++ app/db/database.py
@@ -0,0 +1,8 @@
+from core import settings
+from sqlalchemy import create_engine
+from sqlalchemy.orm import sessionmaker
+
+DATABASE_URL = settings.DATABASE_URL
+
+engine = create_engine(DATABASE_URL)
+SessionLocal = sessionmaker(autocommit=False, autoflush=False, bind=engine)
